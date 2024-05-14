db73b5d9ae499e03028904a686c63e382a9fd318
Kenan Cibooğlu <kenancibooglu@outlook.com>
2024-05-14 21:44:19 +0300

    create project

---
+++ Hastane_Yonetim/Hastane_Yonetim/FrmHastaKayit.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Hastane_Yonetim
+{
+    partial class FrmHastaKayit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.mskTC = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(172, 111);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(110, 31);
+            this.txtSifre.TabIndex = 8;
+            // 
+            // mskTC
+            // 
+            this.mskTC.Location = new System.Drawing.Point(172, 49);
+            this.mskTC.Mask = "00000000000";
+            this.mskTC.Name = "mskTC";
+            this.mskTC.Size = new System.Drawing.Size(110, 31);
+            this.mskTC.TabIndex = 7;
+            this.mskTC.ValidatingType = typeof(int);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sifre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "TC Kimlik No:";
+            // 
+            // FrmHastaKayit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 191);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.mskTC);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "FrmHastaKayit";
+            this.Text = "FrmHastaKayit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.MaskedTextBox mskTC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
